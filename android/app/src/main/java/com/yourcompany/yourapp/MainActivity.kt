d52f42b5a26d809e7568e86c20cd7b4596d0c3a2
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2024-07-15 17:59:21 +0100

    fix: rebuild app to make sure it works with android sdk as well as ios

--- android/app/src/main/java/com/yourcompany/yourapp/MainActivity.kt
+++ android/app/src/main/java/com/yourcompany/yourapp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.theoribeiro.opsqliteexpotursotest
+package com.yourcompany.yourapp
 
 import android.os.Build
 import android.os.Bundle
--- android/app/src/main/java/com/yourcompany/yourapp/MainApplication.kt
+++ android/app/src/main/java/com/yourcompany/yourapp/MainApplication.kt
@@ -1,4 +1,4 @@
-package com.theoribeiro.opsqliteexpotursotest
+package com.yourcompany.yourapp
 
 import android.app.Application
 import android.content.res.Configuration
