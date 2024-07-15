105fc30cde5ad758bd561db934244ce1fce453ed
Theo Ribeiro <10909928+theorib@users.noreply.github.com>
2024-07-15 14:26:46 +0100

    feat: add ability to add and delete items using data from faker-js

--- app/index.tsx
+++ app/index.tsx
@@ -1,36 +1,62 @@
 import React from 'react';
-import { Text, View, FlatList, StyleSheet, ScrollView } from 'react-native';
+import {
+  Text,
+  View,
+  FlatList,
+  StyleSheet,
+  ScrollView,
+  Button,
+  TouchableOpacity,
+} from 'react-native';
 import { openRemote, openSync } from '@op-engineering/op-sqlite';
+import { faker } from '@faker-js/faker';
 
-type Ligth = {
-  id: number;
+const syncDb = openSync({
+  name: 'myDb.sqlite',
+  url: process.env.EXPO_PUBLIC_TURSO_DATABASE_URL!,
+  authToken: process.env.EXPO_PUBLIC_TURSO_AUTH_TOKEN!,
+});
+syncDb.sync();
+
+type Light = {
+  id: string;
   name: string;
-  output: string;
+  output: number;
   image: string | null;
 };
-let lights: Ligth[] = [];
 
-try {
-  const syncDb = openSync({
-    name: 'myDb.sqlite',
-    url: process.env.EXPO_PUBLIC_TURSO_DATABASE_URL!,
-    authToken: process.env.EXPO_PUBLIC_TURSO_AUTH_TOKEN!,
-  });
+const createNewLight = (): Light => {
+  return {
+    id: faker.string.uuid(),
+    name: faker.commerce.productName(),
+    output: faker.number.int({ min: 100, max: 100000 }),
+    image: faker.image.urlPlaceholder({ height: 600, width: 600 }),
+  };
+};
+
+const getAllLights = function (): Light[] | [] {
   syncDb.sync();
-  const syncData = syncDb.execute('SELECT * FROM lights');
+  const syncData = syncDb.execute(`
+    SELECT * FROM lights 
+    ORDER BY substr(id, 15, 4) || substr(id, 10, 4) || substr(id, 1, 8) DESC
+  `);
+  return syncData?.rows?._array || [];
+};
 
-  lights = syncData?.rows?._array || [];
+const addNewLight = (): Light => {
+  const newLight = createNewLight();
+  syncDb.execute(
+    'INSERT INTO lights (id, name, output, image) VALUES (?, ?, ?, ?)',
+    [newLight.id, newLight.name, newLight.output, newLight.image]
+  );
+  syncDb.sync();
+  return newLight;
+};
 
-  // const remoteDb = openRemote({
-  //   url: process.env.EXPO_PUBLIC_TURSO_DATABASE_URL!,
-  //   authToken: process.env.EXPO_PUBLIC_TURSO_AUTH_TOKEN!,
-  // });
-  // const remoteData = remoteDb.execute('SELECT * FROM lights');
-  // lights = remoteData?.rows?._array || [];
-  // console.log(lights);
-} catch (err) {
-  console.error(err);
-}
+const deleteLightById = (id: string) => {
+  syncDb.execute('DELETE FROM lights WHERE id = ?', [id]);
+  syncDb.sync();
+};
 
 const styles = StyleSheet.create({
   scrollContainer: {
@@ -45,7 +71,7 @@ const styles = StyleSheet.create({
   header: {
     fontSize: 24,
     fontWeight: 'bold',
-    marginBottom: 20,
+    marginBottom: 6,
     textAlign: 'center',
   },
   item: {
@@ -66,26 +92,72 @@ const styles = StyleSheet.create({
     textAlign: 'center',
     marginTop: 20,
   },
+  buttonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 20,
+    marginHorizontal: 16,
+    marginBottom: 20,
+  },
+
+  button: {
+    backgroundColor: '#007AFF',
+    paddingVertical: 12,
+    paddingHorizontal: 20,
+    borderRadius: 8,
+    elevation: 3,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
 });
 
 export default function Index() {
-  const renderItem = ({ item }: { item: Ligth }) => (
-    <View style={styles.item}>
-      <Text style={styles.title}>{item.name}</Text>
-      <Text>ID: {item.id}</Text>
-      <Text>Output: {item.output}</Text>
-      <Text>Image: {item.image ? 'Available' : 'Not available'}</Text>
-    </View>
-  );
+  const [lights, setLights] = React.useState<Light[]>(() => getAllLights());
+
+  const handleAddLight = () => {
+    const newLight = addNewLight();
+    setLights(lights => [newLight, ...lights]);
+  };
+
+  const handleDeleteLight = () => {
+    if (lights.length > 0) {
+      const lastLight = lights[lights.length - 1];
+      deleteLightById(lastLight.id);
+      setLights(lights => lights.slice(1));
+    }
+  };
 
   return (
     <ScrollView contentContainerStyle={styles.scrollContainer}>
       <View style={styles.container}>
-        <Text style={styles.header}>Lights List</Text>
+        <Text style={styles.header}>Lights List ({lights.length})</Text>
+        <View style={styles.buttonContainer}>
+          <TouchableOpacity style={styles.button} onPress={handleAddLight}>
+            <Text style={styles.buttonText}>New Light</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.button} onPress={handleDeleteLight}>
+            <Text style={styles.buttonText}>Delete Last </Text>
+          </TouchableOpacity>
+        </View>
         {lights.length > 0 ? (
           <FlatList
             data={lights}
-            renderItem={renderItem}
+            renderItem={({ item }: { item: Light }) => (
+              <View style={styles.item}>
+                <Text style={styles.title}>{item.name}</Text>
+                <Text>ID: {item.id}</Text>
+                <Text>Output: {item.output}</Text>
+                <Text>Image: {item.image ? 'Available' : 'Not available'}</Text>
+              </View>
+            )}
             keyExtractor={item => `${item.id}-id`}
             scrollEnabled={false}
           />
